33e46696f61899ff8c7b20671bd54708e44923cf
Thomas Bush <tomb@khws.co.uk>
2018-01-29 12:52:48 +0000

    Initial commit'

---
+++ js/preload.js
@@ -0,0 +1,121 @@
+(function($) {
+
+    var imgList = [];
+
+    $.extend({
+
+        preload: function(imgArr, option) {
+
+            var setting = $.extend({
+
+                init: function(loaded, total) {},
+
+                loaded: function(img, loaded, total) {},
+
+                loaded_all: function(loaded, total) {}
+
+            }, option);
+
+            var total = imgArr.length;
+
+            var loaded = 0;
+
+            setting.init(0, total);
+
+            for(var i in imgArr) {
+
+                imgList.push($("<img />")
+
+                    .attr("src", imgArr[i])
+
+                    .on('load', function(){
+
+                        loaded++;
+
+                        setting.loaded(this, loaded, total);
+
+                        if(loaded == total) {
+
+                            setting.loaded_all(loaded, total);
+
+                        }
+
+                    })
+
+                );
+
+            }
+
+        }
+
+    });
+
+    $.preload(
+        [
+            "img/logo.png",
+            "img/test.jpg",
+            "img/contact/success.jpg",
+            "img/contact/loading.gif",
+            "img/modal/desktop.jpg",
+            "img/modal/ipad.jpg",
+            "img/modal/screens/ask_xmas/christmas_menu_full_0.jpg",
+            "img/modal/screens/ask_xmas/christmas_menu_full_1.jpg",
+            "img/modal/screens/ask_xmas/christmas_menu_full_2.jpg",
+            "img/modal/screens/easy_view/easy_view_0.jpg",
+            "img/modal/screens/esp/ow_twenty_0.jpg",
+            "img/modal/screens/iws/iws_full_0.jpg",
+            "img/modal/screens/iws/iws_full_1.jpg",
+            "img/modal/screens/iws/iws_full_2.jpg",
+            "img/modal/screens/iws/iws_full_3.jpg",
+            "img/modal/screens/iws/iws_full_4.jpg",
+            "img/modal/screens/iws/iws_full_5.jpg",
+            "img/modal/screens/light_duty/ld_0.jpg",
+            "img/modal/screens/light_duty/ld_1.jpg",
+            "img/modal/screens/light_duty/ld_2.jpg",
+            "img/modal/screens/light_duty/ld_3.jpg",
+            "img/modal/screens/light_duty/ld_4.jpg",
+            "img/modal/screens/light_duty/ld_5.jpg",
+            "img/modal/screens/meg/engine_guarantee_full_0.jpg",
+            "img/modal/screens/meg/engine_guarantee_full_1.jpg",
+            "img/modal/screens/meg/engine_guarantee_full_2.jpg",
+            "img/modal/screens/meg/engine_guarantee_full_3.jpg",
+            "img/modal/screens/meg/engine_guarantee_full_4.jpg",
+            "img/modal/screens/meg/engine_guarantee_full_5.jpg",
+            "img/modal/screens/mpa/mpa_full_0.jpg",
+            "img/modal/screens/mpa/mpa_full_1.jpg",
+            "img/modal/screens/mpa/mpa_full_2.jpg",
+            "img/modal/screens/mpa/mpa_full_3.jpg",
+            "img/modal/screens/smart_hub/smart_hub_0.jpg",
+            "img/modal/screens/smart_hub/smart_hub_1.jpg",
+            "img/modal/screens/smart_hub/smart_hub_2.jpg",
+            "img/modal/screens/smart_hub/smart_hub_3.jpg",
+            "img/modal/screens/smart_hub/smart_hub_4.jpg",
+            "img/modal/screens/the_frame/the_frame_full_0.jpg"
+
+        ], 
+        {
+        init: function(loaded, total) {
+            console.log("Loading "+loaded+" of "+total+" images");
+        },
+        loaded: function(img, loaded, total) {
+            console.log("Loading "+loaded+" of "+total+" images");
+        },
+
+        loaded_all: function(loaded, total) {
+
+            // min load time is 2 seconds
+            setTimeout(function(){
+              
+                $(".preloader").addClass("preloader--hidden").delay(300).queue(function(){
+
+                    $(this).hide().parent().removeClass("noScrolling");
+
+                });
+
+            }, 2000);
+
+        }
+
+    });
+
+})(jQuery);
---
+++ js/script.js
@@ -0,0 +1,210 @@
+$(document).ready(function() {
+
+	// open/close navigation
+	$('body').on('click', 'div.hamburger--nav, .nav a', function() {
+
+	    $(".hamburger--nav").toggleClass("hamburger--visible");
+
+	    $(".nav").toggleClass("nav--visible");
+
+	    checkScroll();
+
+	});
+
+	// nav click to anchors
+	$(".nav a, .header-anchor").click(function(e) {
+
+		e.preventDefault();
+
+		var target = $(this.hash);
+
+		$('html,body').delay(250).animate({scrollTop: target.offset().top},'slow');
+
+		console.log(e);
+
+	});
+
+	// flip work cards
+	$(".card .front").click(function() {
+
+		$(this).parent().toggleClass("card--flipped").siblings().removeClass("card--flipped");
+
+	});
+
+	// open work lightbox
+	$(".work .card .back .click").click(function() {
+
+		var	getData = $(this).parent(),
+		 	subject = getData.attr("data-subject"),
+		 	title 	= getData.prev().find("h2").text(),
+		 	screen  = getData.attr("data-screen"),
+			image 	= getData.attr("data-image"),
+			opts	= getData.data("opts");
+
+		$("main").append("<div class='modal'><div class='subject subject--" + subject + "'><div class='sec text'><h2>" + title + "</h2></div><div class='sec image " + screen + "'><div class='scrollable'><img data-opts='" + opts + "' src='img/modal/screens/" + subject + "/" + image + "'></div><div class='change_image left'></div><div class='change_image right'></div></div></div></div>");
+
+		// if modal only has one image
+		if ( opts === 0 ) {
+
+			$(".modal .change_image").addClass("disabled");
+
+		}
+
+		$(".hamburger").toggleClass("hamburger--nav hamburger--modal").addClass("hamburger--visible");
+
+		checkScroll();
+
+	});
+
+	// change lightbox image
+	$(document).on('click touch touchstart', '.modal .image .change_image', function() {
+
+		var target  = $(this).siblings(".scrollable").find("img"),
+			limit  	= target.data("opts"),
+			src     = target.attr("src"),
+			srcInt  = src.match(/\d+/),
+			num     = parseInt(srcInt);
+
+		// left
+		if ( $(this).hasClass("left") ) {
+
+			if ( num === 0 ) {
+
+				num = limit;	
+
+			} else { num--; }
+
+			target.attr('src', src.replace(/\d+/, num));
+
+		}
+		// right
+		else {
+
+			if ( num === limit ) {
+
+				num = 0;	
+
+			} else { num++; }
+
+			target.attr('src', src.replace(/\d+/, num));
+
+		}
+
+		$(target).parent().scrollTop(0);
+
+	});
+
+	// close work lightbox
+	$(document).on('click', 'div.hamburger--modal', function() {
+
+		$("main .modal").remove();
+
+		$(this).toggleClass("hamburger--nav hamburger--modal").removeClass("hamburger--visible");
+
+		checkScroll();
+
+	});
+
+	// disable scroll
+	function checkScroll() {
+
+		var distance = $(window).scrollTop();
+
+
+		if ( $(".modal").length > 0 || $(".nav--visible").length > 0) {
+
+			$("body").addClass("noScrolling");
+
+			stopScroll(true);
+
+		} else {
+
+			$("body").removeClass("noScrolling");
+
+			stopScroll(false);
+
+		}
+
+	}
+
+	// mobile scrolling
+	function stopScroll (bool) {
+
+	    if (bool === true) {
+
+	        $(document).on('touchmove',function(e) { e.preventDefault(); });
+
+
+	    } else { $(document).off('touchmove'); }
+	}
+
+	$(window).on("mousewheel DOMMouseScroll", function(e){
+	    // fixes modal scroll on firefox
+	});
+
+	// send email failed
+	$("#send").click(function(){
+
+	    $(this).closest('form').find('[required]').addClass('required');
+
+	});
+
+	// send email
+	$(function() {
+
+	    $('form').on('submit', function(e) {
+        
+        	e.preventDefault();
+
+        	// add loading wheel
+        	$(".formDiv").removeClass("result--failure result--success").addClass("result result--loading");
+
+        	// capture data
+        	var data = {
+			    name: $("#name").val(),
+			    email: $("#email").val(),
+			    message: $("#message").val()
+			};
+
+			// send email
+			$.ajax({
+
+			    url: "https://formspree.io/thomasjjbush@gmail.com", 
+			    method: "POST",
+			    data: data,
+			    dataType: "json",
+
+			    // add tick
+			    success: function(){
+			        $('.result').removeClass("result--loading result--failure").addClass("result--success");
+			    },
+
+			    // add cross
+			    error: function() { 
+                    $('.result').removeClass("result--loading result--success").addClass("result--failure"); 
+                }
+
+			});
+
+	        return false;
+
+	    });
+
+	});
+
+});
+
+// fix to assign 1:1 ratio height to cards within flexbox
+$(window).on("load resize",function(){
+
+	var card  = $(".work .card"),
+	 	width = card.width();
+
+	$(card).css("height", width)
+
+});
+
+
+
+
+	
